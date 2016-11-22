7d6c05ae4d7d34a7a3f13b1581d6bb8a8559d8f0
Alexey Kostromin <dejordens@gmail.com>
2016-11-22 22:20:45 +0200

    Added Nightly test

---
+++ Selenium/Selenium/LoginWithNightly.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium
+{
+    class LoginWithNightly
+    {
+        protected static IWebDriver driver;
+        protected static WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+           FirefoxOptions options = new FirefoxOptions(); 
+           options.BrowserExecutableLocation = @"c:\Program Files\Nightly\firefox.exe";
+           driver = new FirefoxDriver(options);
+           wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void LoginTest()
+        {
+            string LifecartLoginPage = "http://localhost/litecart/admin/";
+            string CssLogin = "[type=\"text\"]";
+            string CssPassword = "[type=\"password\"]";
+            string CssLoginBtn = "[name=\"login\"]";
+
+            string Login = "admin";
+            string Password = "admin";
+
+            driver.Url = LifecartLoginPage;
+            driver.FindElement(By.CssSelector(CssLogin)).SendKeys(Login);
+            driver.FindElement(By.CssSelector(CssPassword)).SendKeys(Password);
+            driver.FindElement(By.CssSelector(CssLoginBtn)).Click();
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(AdminHomePage.CssLitecartLogo)));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}

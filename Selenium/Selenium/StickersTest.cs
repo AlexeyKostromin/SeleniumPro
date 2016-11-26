cc64c62ef3ae2a5206645812544d327a7e3e6e82
Alexey Kostromin <dejordens@gmail.com>
2016-11-26 12:32:35 +0200

    EveryItemHasStickerTest added

---
+++ Selenium/Selenium/StickersTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Selenium
+{
+    public class StickersTest : Base
+    {
+
+        [Test]
+        public void EveryItemHasStickerTest()
+        {
+            driver.Url = "http://localhost/litecart/en/";
+            var products = driver.FindElements(By.CssSelector("[class^=product]"));
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                var sticker = products[i].FindElements(By.CssSelector(".sticker"));
+                Assert.IsTrue(sticker.Count == 1, "item {0} has {1} stickers, but should have only 1", i, sticker.Count);
+
+            }
+        }
+    }
+}
